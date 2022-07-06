658ca62570400226e16690f7af317484c121298e
WADIGZON DIAZ-WONG <wadigzon@users.noreply.github.com>
2022-07-06 15:25:16 -0400

    Update number-to-ordinal.js
    forgot digit

--- number-to-ordinal.js
+++ number-to-ordinal.js
@@ -1,4 +1,5 @@
 const numberToOrdinal = number => {
     let end = ['st', 'nd', 'rd', 'th'];
+    const digit = number % 10;
     return number > 10 && number < 20 ? `${number}${end[3]}` : ( digit > 0 && digit < 4 ? `${number}${end[digit-1]}` : `${number}${end[3]}` );
 };
