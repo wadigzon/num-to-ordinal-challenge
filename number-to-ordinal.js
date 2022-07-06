513d06ba15e0c495f0f72f92c3652d076003fe98
WADIGZON DIAZ-WONG <wadigzon@users.noreply.github.com>
2022-07-06 15:13:45 -0400

    Update number-to-ordinal.js
    short and sweet, no need for extra functions within and no need for array creation and array function calls (join, clone).

--- number-to-ordinal.js
+++ number-to-ordinal.js
@@ -1,48 +1,19 @@
 const numberToOrdinal = number => {
-  let end = ['st', 'nd', 'rd', 'th']
-  let numberArr = number.toString().split("")
-  let ordinal; 
-  
-  // checking for zero only
-  const checkZero = num => num === 0 ? true : false;
-  
-  // checking for the teen numbers
-  const checkTeens = numArr => {
-    let clonedArr = [...numArr] 
-    let teens = ['11', '12', '13', '14', '15', '16', '17', '18', '19']
-    let teen = clonedArr.splice(-2).join("")
-    if (teens.includes(teen)) {
-      return true;
-    }
-    return false;
-  }
-  
-  // check for other numbers
-  const checkOthers = numArr => {
-    let clonedArr = [...numArr]
-    let digit = clonedArr.pop()
-    
-    let result;
-    if (digit === "1"){
-      result = `${number}${end[0]}`
-    } else if (digit === "2") {
-      result = `${number}${end[1]}`
-    } else if (digit === "3") {
-      result = `${number}${end[2]}`
+    let end = ['st', 'nd', 'rd', 'th'];
+    let ordinal; 
+    if (number > 10 && number < 20) {
+        ordinal = `${number}${end[3]}`;
     } else {
-      result = `${number}${end[3]}`
+        const digit = number % 10;
+        if (digit === 1){
+            ordinal = `${number}${end[0]}`
+        } else if (digit === 2) {
+            ordinal = `${number}${end[1]}`
+        } else if (digit === 3) {
+            ordinal = `${number}${end[2]}`
+        } else {
+            ordinal = `${number}${end[3]}`
+        }
     }
-    return result;
-  }  
-  
-  if (checkZero(number) === true) {
-    ordinal = number.toString()
-  } else if (checkTeens(numberArr)) {
-    ordinal = `${number}${end[3]}`
-  } else {
-    return checkOthers(numberArr)
-  }
-  
-  
-  return ordinal; 
+    return ordinal; 
 };
